84dbef421727dc06407a50457b173fe8a52ae087
Safak Gezer <safakge@gmail.com>
2020-08-26 19:16:37 +0300

    1 finished

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        HStack() {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUp:Bool
+    var body: some View {
+        ZStack() {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .padding()
+            .accentColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
     }
 }

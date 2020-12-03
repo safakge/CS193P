1ec39664a609150399a452433dbf87d3452d3cff
Safak Gezer <safakge@gmail.com>
2020-12-03 16:49:40 +0300

    Added the SetGame proj

---
+++ SetGame/SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Safak Gezer on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

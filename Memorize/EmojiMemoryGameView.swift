ab779a4eef56474a744326d042546bff0c5802c2
Safak Gezer <safakge@gmail.com>
2020-12-03 12:25:30 +0300

    Nothing

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -46,7 +46,7 @@ struct CardView: View {
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                Pie(startAngle: Angle.degrees(0 - 90), endAngle: Angle.degrees(10 - 90), clockwise: true).padding(5).opacity(0.4)
+                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true).padding(5).opacity(0.4)
                 Text(self.card.content)
                     .font(Font.system(size: fontSize(for: size)))
                     .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0)) // do a somersault when matched
